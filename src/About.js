3b04343bf9489ce130f0fde26a046edd338fdeec
Christophe Soares <soareschristophe@gmail.com>
2024-03-05 12:35:57 +0000

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./About.css";
+
+class About extends Component {
+  render() {
+    return <div className="about-section">About</div>;
+  }
+}
+
+export default About; // Don’t forget to use export default!
